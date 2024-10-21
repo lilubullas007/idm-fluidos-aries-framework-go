b08f9a69fb7ac2ed3c86ff23f51991dfa1954fbc
Jose Luis <lilu2001x@gmail.com>
2024-10-21 20:19:03 +0200

    New poc/command_test.go tests

--- pkg/controller/command/poc/command_test.go
+++ pkg/controller/command/poc/command_test.go
@@ -30,9 +30,13 @@ import (
 	"github.com/square/go-jose/v3/json"
 	"github.com/stretchr/testify/require"
 
-	"github.com/hyperledger/aries-framework-go/pkg/internal/logutil"
+	"net/http"
+	"net/http/httptest"
+	"time"
+
 	mockvdr "github.com/hyperledger/aries-framework-go/pkg/mock/vdr"
 	"github.com/hyperledger/aries-framework-go/pkg/wallet"
+	"github.com/stretchr/testify/assert"
 
 	"testing"
 )
@@ -109,31 +113,90 @@ func TestNewDID(t *testing.T) {
 	})*/
 }
 
+func TestDoDeviceEnrolment(t *testing.T) {
+	t.Run("test DoDeviceEnrolment method - success", func(t *testing.T) {
+		const (
+			sampleUser1 = "sampleUser1"
+			samplePass  = "fakepassphrase"
+			sampleDID   = "did:example:123"
+			sampleURL   = "https://issuer:9082"
+		)
+
+		// Argumentos de prueba para DoDeviceEnrolment
+		enrolmentArgs := DoDeviceEnrolmentArgs{
+			Url: sampleURL,
+			IdProofs: []IdProof{
+				{AttrName: "holderName", AttrValue: "FluidosNode"},
+				{AttrName: "fluidosRole", AttrValue: "Customer"},
+				{AttrName: "deviceType", AttrValue: "Server"},
+				{AttrName: "orgIdentifier", AttrValue: "FLUIDOS_id_23241231412"},
+				{AttrName: "physicalAddress", AttrValue: "50:80:61:82:ab:c9"},
+			},
+		}
+
+		mockServer := setupMockEnrolmentServer(t)
+		defer mockServer.Close()
+		enrolmentArgs.Url = mockServer.URL
+
+		// Serializar el request para simular entrada válida
+		var requestBody bytes.Buffer
+		err := json.NewEncoder(&requestBody).Encode(enrolmentArgs)
+		require.NoError(t, err)
+
+		// Crear mocks
+		vcwalletCommand := vcwallet.New(newMockProvider(t), &vcwallet.Config{})
+		require.NotNil(t, vcwalletCommand)
+		require.NoError(t, err)
+
+		vdrCommand, err := vdr.New(&mockprovider.Provider{
+			StorageProviderValue: mockstore.NewMockStoreProvider(),
+			VDRegistryValue:      &mockvdr.MockVDRegistry{}, // Mock de VDRegistry
+		})
+		require.NoError(t, err)
+
+		// Crear nueva instancia de Command
+		command, err := New(vdrCommand, vcwalletCommand)
+		require.NoError(t, err)
+
+		// Crear writer para capturar la salida
+		var l bytes.Buffer
+
+		// Ejecutar DoDeviceEnrolment
+		err = command.DoDeviceEnrolment(&l, &requestBody)
+		require.NoError(t, err)
+
+		// Validar la respuesta decodificada
+		var response DoDeviceEnrolmentResult
+		err = json.NewDecoder(&l).Decode(&response)
+		require.NoError(t, err)
+		require.NotNil(t, response)
+
+		// Imprimir la credencial obtenida
+		fmt.Println("Credential storage ID:", response.CredStorageId)
+		fmt.Println("Credential:", string(response.Credential))
+
+		var cred map[string]interface{}
+
+		// Decodificar contenido de la credencial
+		err = json.Unmarshal(response.Credential, &cred)
+		require.NoError(t, err)
+
+		// Mostrar el resultado en formato legible
+		prettyCred, err := json.MarshalIndent(cred, "", "  ")
+		require.NoError(t, err)
+
+		fmt.Printf("Formatted Credential: %s\n", string(prettyCred))
+
+		fmt.Println()
+	})
+}
+
 func TestGetVCredential(t *testing.T) {
 	t.Run("test GetVCredential method - success", func(t *testing.T) {
-		/**
-		// Sample VC
-		vc := `{
-			"@context": ["https://www.w3.org/2018/credentials/v1"],
-			"id": "http://example.edu/credentials/1872",
-			"type": ["VerifiableCredential"],
-			"issuer": {
-				"id": "did:example:123"
-			},
-			"credentialSubject": {
-				"id": "did:example:456",
-				"name": "John Doe"
-			}
-		}`
-
-		// Crear el almacenamiento simulado con la credencial verificable
-		storeData := make(map[string]mockstore.DBEntry)
-		storeData["sampleCredId"] = mockstore.DBEntry{Value: []byte(vc)}
-		**/
-
 		const (
-			sampleUser1  = "sampleUser1"
-			sampleCredId = "http://example.edu/credentials/1872"
+			sampleUser1    = "sampleUser1"
+			sampleCredId   = "http://example.edu/credentials/1872"
+			fakePassphrase = "fakepassphrase"
 		)
 
 		fmt.Printf("ID of the sought credential: %s\n", sampleCredId)
@@ -173,22 +236,16 @@ func TestGetVCredential(t *testing.T) {
 			},
 		}
 
-		// Create test profile
-		err = command.createSampleUserProfile(t, sampleUser1, "fakepassphrase")
-		if err != nil {
-			logutil.LogInfo(logger, "createProfileCommand", "createProfile", "Error al crear el perfil:", err.Error())
-		}
+		// Create sample profile
+		err = command.createSampleUserProfile(t, sampleUser1, fakePassphrase)
+		require.NoError(t, err)
 
-		var token1 string
-		var lock1 func()
-		token1, lock1 = command.unlockWallet(t, sampleUser1, "fakepassphrase")
+		token1, lock1 := command.unlockWallet(t, sampleUser1, fakePassphrase)
 		defer lock1()
 
 		// Add credential to wallet
 		err = command.AddCredentialToWallet(sampleUser1, token1, wallet.Credential, vc2, "")
-		if err != nil {
-			logutil.LogInfo(logger, "addToWalletCommand", "addToWallet", "Error al añadir la credencial al wallet:", err.Error())
-		}
+		require.NoError(t, err)
 
 		// GetVCredential method
 		err = command.GetVCredential(&l, reader)
@@ -218,6 +275,109 @@ func TestGetVCredential(t *testing.T) {
 		fmt.Println()
 	})
 }
+
+/**
+func TestGenerateVP(t *testing.T) {
+	t.Run("test GenerateVP method - success", func(t *testing.T) {
+		const (
+			sampleUser     = "sampleUser1"
+			sampleCredId   = "http://example.edu/credentials/1872"
+			fakePassphrase = "fakepassphrase"
+		)
+
+		// Define request arguments for GenerateVP
+		getVPArgs := GenerateVPArgs{
+			CredId: sampleCredId,
+			QueryByFrame: QueryByFrame{
+				Frame: FrameFluidos{
+					Context: []string{
+						"https://www.w3.org/2018/credentials/v1",
+						"https://www.w3.org/2018/credentials/examples/v1",
+						"https://ssiproject.inf.um.es/security/psms/v1",
+						"https://ssiproject.inf.um.es/poc/context/v1",
+					},
+					Type: []string{
+						"VerifiableCredential",
+						"FluidosCredential",
+					},
+					Explicit: true,
+					// Declare the attributes we want to disclose
+					CredentialSubject: map[string]interface{}{
+						"@explicit":   true,       // Ensure only specified fields are disclosed
+						"fluidosRole": struct{}{}, // Not revealing the actual content
+						"holderName":  struct{}{}, // Not revealing the actual content
+					},
+				},
+			},
+		}
+		var l bytes.Buffer
+		reader, err := getReader(getVPArgs)
+		require.NoError(t, err)
+		require.NotNil(t, reader)
+
+		// Mocks (vcwallet and vdr)
+		vcwalletCommand := vcwallet.New(newMockProvider(t), &vcwallet.Config{})
+		require.NotNil(t, vcwalletCommand)
+
+		vdrCommand, err := vdr.New(&mockprovider.Provider{
+			StorageProviderValue: mockstore.NewMockStoreProvider(),
+			VDRegistryValue:      &mockvdr.MockVDRegistry{},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, vdrCommand)
+
+		command, err := New(vdrCommand, vcwalletCommand)
+		require.NoError(t, err)
+		require.NotNil(t, command)
+
+		// Create sample profile and unlock wallet
+		err = command.createSampleUserProfile(t, sampleUser, fakePassphrase)
+		require.NoError(t, err)
+
+		token1, lock1 := command.unlockWallet(t, sampleUser, fakePassphrase)
+		defer lock1()
+
+		// Add credential to wallet
+		vc := map[string]interface{}{
+			"@context":     []string{"https://www.w3.org/2018/credentials/v1"},
+			"id":           sampleCredId,
+			"type":         []string{"VerifiableCredential", "FluidosCredential"},
+			"issuer":       map[string]interface{}{"id": "did:example:123"},
+			"issuanceDate": "2024-10-21T09:00:00Z",
+			"credentialSubject": map[string]interface{}{
+				"id":          "did:example:456",
+				"name":        "John Doe",
+				"fluidosRole": "Admin",
+				"holderName":  "John Doe",
+			},
+		}
+		err = command.AddCredentialToWallet(sampleUser, token1, wallet.Credential, vc, "")
+		require.NoError(t, err)
+
+		// Ejecutar el método GenerateVP
+		err = command.GenerateVP(&l, reader)
+		require.NoError(t, err)
+
+		// Validar el resultado
+		var response GenerateVPResultCustom
+		err = json.NewDecoder(&l).Decode(&response)
+		require.NoError(t, err)
+
+		require.NotNil(t, response)
+		fmt.Printf("Verifiable Presentation response: %+v\n", response)
+
+		// Verificar el contenido de la presentación
+		var vp map[string]interface{}
+		err = json.Unmarshal(*response.Results[0], &vp)
+		require.NoError(t, err)
+
+		prettyVP, err := json.MarshalIndent(vp, "", "  ")
+		require.NoError(t, err)
+
+		fmt.Printf("Verifiable Presentation: %s\n", string(prettyVP))
+	})
+}
+**/
 
 func readDIDtesting(t *testing.T) {
 
@@ -314,4 +474,55 @@ func (o *Command) AddCredentialToWallet(userID string, walletAuth string, conten
 	}
 
 	return nil
+}
+
+func setupMockEnrolmentServer(t *testing.T) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Asegurar que la URL es la esperada
+		assert.Equal(t, "/fluidos/idm/acceptEnrolment", req.URL.Path)
+
+		// Generar las fechas dinámicas
+		issuanceDate := time.Now().Format(time.RFC3339Nano)
+		expirationDate := time.Now().AddDate(0, 0, 1).Format(time.RFC3339Nano) // Añadir un día para la fecha de expiración
+
+		// Simular la respuesta de enrolamiento
+		response := map[string]interface{}{
+			"credential": map[string]interface{}{
+				"@context": []string{
+					"https://www.w3.org/2018/credentials/v1",
+					"https://www.w3.org/2018/credentials/examples/v1",
+					"https://ssiproject.inf.um.es/security/psms/v1",
+					"https://ssiproject.inf.um.es/poc/context/v1",
+				},
+				"credentialSubject": map[string]interface{}{
+					"DID":             "did:fabric:DuYKXxLuWnTQzBaa9p1eT1Aya98nirASjVN8dphJjYw",
+					"deviceType":      "Server",
+					"fluidosRole":     "Customer",
+					"holderName":      "FluidosNode",
+					"orgIdentifier":   "FLUIDOS_id_23241231412",
+					"physicalAddress": "50:80:61:82:ab:c9",
+				},
+				"expirationDate": expirationDate,
+				"id":             "did:fabric:IDDOde0vxswhzfNEQwp05_B209NZ8Ssf3IHHhlrt7Ho3068409",
+				"issuanceDate":   issuanceDate,
+				"issuer":         "did:fabric:IDDOde0vxswhzfNEQwp05_B209NZ8Ssf3IHHhlrt7Ho",
+				"proof": map[string]interface{}{
+					"created":            issuanceDate,
+					"proofPurpose":       "assertionMethod",
+					"proofValue":         "BBVHMPF6Alk2zs934vLFUMg6p83X6TsHI1sE_FJ6GX2AHaFDaXuqZ8PZuQHeRRUKYBIXd2TfGmUvZkec77bXhtEL_yo2wtHiX8vMWUDWQ_fzZ4Y6QG9FYJM2wzaexf43xRiyvYWxiONADhz3sNQHILrHgrSVP1fLyGMIocrrQaGs3xf0-ydEUdfCkpsQNZcFmwMuHh_oUC3MJ5RdkkImP6HIruU-Ke7fM4VYcfnd-Pq7FvwfmSDF33Xbn3Zs0vfp2AQY2WKP1X8IcEuLMea6_0YPHhNRdNn-PA-cUqZXPCDQ2uL40Kud6AdCn7Nms3G5ztUKPD50CXzch8PbyPdVw_mjZYpWEd2xwxc4JIjXeXGPKJo8fjxeQHf5aVyn1HCCg7IIo1UMt-M921Z-hllYZuMrGIOTINKYhPjxBKSXwWw4UTt55k0xzZcrtTM8oFCk0j8Vo8mhjM7albUekUfoQwMaw9xvo9pdYIM9v55B2ooQ8ivVCzNSWoAJIj_YrnyMCfeE0vcYnnYUE_j69Whok_c2FuRODPv1TFnICWk6FGC5bhfxF-xMVT4Pt6uyfWn86os",
+					"type":               "PsmsBlsSignature2022",
+					"verificationMethod": "did:fabric:IDDOde0vxswhzfNEQwp05_B209NZ8Ssf3IHHhlrt7Ho#xcrh0rw2Gxlx-SZRQQhi5h4YJ9_VEv_hV9X7sV2unlI",
+				},
+				"type": []string{
+					"VerifiableCredential",
+					"FluidosCredential",
+				},
+			},
+			"credStorageId": "did:fabric:IDDOde0vxswhzfNEQwp05_B209NZ8Ssf3IHHhlrt7Ho3068409",
+		}
+
+		rw.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(rw).Encode(response)
+		require.NoError(t, err)
+	}))
 }
